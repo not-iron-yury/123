ff69255e7e79500789358dbdcee890c285faf948
Yury Notiron <not.iron.yuriy@inbox.ru>
2024-12-02 15:38:14 +0300

    Секция с видео
    Changes to be committed:
    	new file:   src/files/video.jpg
    	new file:   src/files/video.mp4
    	new file:   src/files/video.webm
    	modified:   src/html/index.html
    	new file:   src/html/page-home/video.html
    	new file:   src/img/svg/sprite/play-btn.svg
    	new file:   src/img/svg/sprite/sound-off.svg
    	modified:   src/js/index.js
    	new file:   src/js/modules/video.js
    	modified:   src/scss/UI/icons.scss
    	modified:   src/scss/UI/titles.scss
    	modified:   src/scss/base/_index.scss
    	modified:   src/scss/base/base.scss
    	modified:   src/scss/base/utils.scss
    	modified:   src/scss/base/vars.scss
    	modified:   src/scss/blocks/_index.scss
    	new file:   src/scss/blocks/videoblock.scss

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,6 @@
 import TransferElements from './modules/transfer-nav.js';
 import productQuantity from './modules/product-quantity.js';
 import mobileNavOpenClose from './modules/mobile-nav.js';
+import videoButtons from './modules/video.js';
 
-mobileNavOpenClose();
-productQuantity();
-
-new TransferElements({
-  sourceElement: document.querySelector('.nav__list_top'),
-  breakpoints: {
-    960: {
-      targetElement: document.querySelector('.header__nav_bottom'),
-      targetPosition: 1,
-    },
-  },
-});
+videoButtons();
---
+++ src/js/modules/video.js
@@ -0,0 +1,33 @@
+function videoButtons() {
+  const video = document.querySelector('.videoblock__video');
+
+  if (video) {
+    const playButton = document.querySelector('.videoblock__play-btn');
+    const muteButton = document.querySelector('.videoblock__btn-muted');
+    const overlay = document.querySelector('.videoblock');
+
+    video.addEventListener('click', e => {
+      overlay.classList.add('active');
+      if (video.paused) {
+        video.play();
+        playButton.style.display = 'none';
+      } else {
+        video.pause();
+        playButton.style.display = 'flex';
+      }
+    });
+
+    playButton.addEventListener('click', () => {
+      video.play();
+      playButton.style.display = 'none';
+      overlay.classList.add('active');
+    });
+
+    muteButton.addEventListener('click', () => {
+      video.muted = !video.muted;
+      muteButton.classList.toggle('active');
+    });
+  }
+}
+
+export default videoButtons;
